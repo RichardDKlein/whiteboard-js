39a66c45eba0b87f1aa6a06de3663c7828140b44
RichardDKlein <RichardDKlein@gmail.com>
2024-02-02 17:16:56 -0600

    Tweak JS Doc for array-subsets

--- src/arrays/array-subsets.js
+++ src/arrays/array-subsets.js
@@ -1,7 +1,8 @@
 /**
- * Partitions an array of integers into disjoint subarrays `arrA` and `arrB`
+ * Partitions an array `a` of integers into disjoint subarrays `arrA` and `arrB`
  * such that `arrA` is the smallest possible subarray whose elements sum to a
- * greater number than the elements of `arrB`.
+ * greater number than the elements of `arrB`. (Note that neither `arrA` nor
+ * `arrB` need consist of consecutive elements from `a`.)
  *
  * The strategy is to first sort the given array in descending order. Then,
  * starting from the first element, keep adding elements to `arrA` until the
@@ -11,7 +12,7 @@
  * Performance is O(n*log(n)), due to the sorting operation.
  *
  * @param {number[]} a The array to be partitioned.
- * @returns {number[]} The subarray `arrA`, whose elements are in ascending
+ * @returns {number[]} The subarray `arrA`, with its elements in ascending
  * order.
  */
 export function arraySubsets(a) {
