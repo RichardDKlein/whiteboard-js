5201172e8bf2518f6fdc1f4637572265556404ea
RichardDKlein <RichardDKlein@gmail.com>
2024-02-16 18:52:56 -0600

    Add string-test-utils

---
+++ test/strings/string-test-utils.js
@@ -0,0 +1,28 @@
+/**
+ * Prints a long string, automatically wrapping lines at space characters
+ * to ensure the entire string fits within the visible screen area.
+ *
+ * @param {string} s - The long string to be printed.
+ */
+function printLongString(s) {
+  const maxLen = 68;
+  let currIndex = 0;
+
+  while (currIndex < s.length) {
+    let nextIndex = Math.min(currIndex + maxLen, s.length);
+    let line = s.slice(currIndex, nextIndex);
+
+    // If the next line would exceed maxLen and doesn't end with a space,
+    // find the last space within the maxLen range and break there instead.
+    if (nextIndex < s.length && s.charAt(nextIndex) !== " ") {
+      const lastSpace = line.lastIndexOf(" ");
+      if (lastSpace !== -1) {
+        nextIndex = currIndex + lastSpace + 1;
+        line = s.slice(currIndex, nextIndex);
+      }
+    }
+
+    console.log(line);
+    currIndex = nextIndex;
+  }
+}
